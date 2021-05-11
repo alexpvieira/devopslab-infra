47fe2b43cd9f4667f12e38362475fce5c317b710
Alex Vieira <alex.p.vieira@gmail.com>
2021-05-10 22:40:37 -0300

    Terraform GCP Basics

---
+++ providers.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.64.0"
+    }
+  }
+}
+
+# Configura o Provider Google Cloud com o Projeto
+provider "google" {
+
+  credentials = file("/vagrant/lab-devops-cloud-313114-30184d77876c.json")
+
+  project = "lab-devops-cloud-313114"
+  region  = "us-west1"
+  zone    = "us-west1-b"
+}
